76878443349a6c4edf441d0b6b8ae5f001c512f5
Andrew Savelev <savelevcorr@yandex.ru>
2017-02-26 02:05:45 +0300

    Setup build process

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const GULP = require('gulp');
+const CSSO = require('postcss-CSSO');
+const POSTCSS = require('gulp-postcss');
+const CSSNEXT = require('postcss-cssnext');
+const AUTOPREFIXER = require('autoprefixer');
+const BROWSER_SYNC = require('browser-sync').create();
+
+GULP.task('css', function() {
+	let plugins = [
+	    CSSNEXT,
+	    AUTOPREFIXER({
+	    	browsers: ['> 1%'], 
+	    	cascade: false
+	    }),
+	    CSSO
+    ];
+  
+  	return GULP.src('src/*.css')
+		.pipe(POSTCSS(plugins))
+		.pipe(GULP.dest('css'))
+		.pipe(BROWSER_SYNC.stream());
+});
+
+// Static Server + watching css/html files
+GULP.task('serve', ['css'], function() {
+
+    BROWSER_SYNC.init({
+        server: "./example"
+    });
+
+    GULP.watch("src/*.css", ['css']);
+    GULP.watch("example/*.html").on('change', BROWSER_SYNC.reload);
+});
